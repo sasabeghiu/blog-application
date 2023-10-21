0e737b42ddb47b889e2a986bcc7c7a75cf11933d
Sasha <646415@student.inholland.nl>
2023-10-21 18:35:47 +0200

    update index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,6 +5,11 @@
         <h1>{{ $title }}</h1>
         <p>This is the Laravel application from "Laravel from scratch" course.</p>
         <p>
+            This application is fully written in Laravel using Docker. The scope of this application is to implement the
+            CRUD functionality on a Blog Application. You can register or log in as a user and create, update or delete a
+            post made by you. You can also see the posts made by other users, wirhout being logged in.
+        </p>
+        <p>
             <a href="/login" class="btn btn-primary" role="button">Login</a>
             <a href="/register" class="btn btn-success" role="button">Register</a>
         </p>
